9bf2d6be7466637c40f74d35aeb0a78931e3d283
Aayush Thapa <a1thapa@ryerson.ca>
2022-04-14 21:32:44 -0400

    add application chaincode

---
+++ chaincode/Application/lib/chaincode.js
@@ -0,0 +1,90 @@
+/*
+ * Copyright IBM Corp. All Rights Reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+const { Contract } = require('fabric-contract-api');
+
+class Application extends Contract {
+
+    async initLedger(ctx) {
+        const houses = [
+            {
+                price: 1000000,
+                location: '2155 Lawrence Avenue East',
+                type: 'House',
+                owner: 'Alton',
+            },
+            {
+                price: 500000,
+                location: '2100 Lawrence Avenue East',
+                type: 'Condo',
+                owner: 'Aayush',
+            },
+            {
+                price: 1500000,
+                location: '34 Kennedy Road',
+                type: 'House',
+                owner: 'Cindy',
+            }
+        ];
+
+        for (let i = 0; i < houses.length; i++) {
+            houses[i].docType = 'house';
+            await ctx.stub.putState('HOUSE' + i, Buffer.from(JSON.stringify(houses[i])));
+            console.info('Added <--> ', houses[i]);
+        }
+    }
+
+    async createHouse(ctx, houseID, price, location, type, owner) {
+
+        const house = {
+            price,
+            docType: 'house',
+            location,
+            type,
+            owner,
+        };
+
+        await ctx.stub.putState(houseID, Buffer.from(JSON.stringify(house)));
+    }
+
+    async queryAllHouses(ctx) {
+        const startKey = '';
+        const endKey = '';
+        const allResults = [];
+        for await (const {key, value} of ctx.stub.getStateByRange(startKey, endKey)) {
+            const strValue = Buffer.from(value).toString('utf8');
+            let record;
+            try {
+                record = JSON.parse(strValue);
+            } catch (err) {
+                console.log(err);
+                record = strValue;
+            }
+            allResults.push({ Key: key, Record: record });
+        }
+        return JSON.stringify(allResults);
+    }
+
+    async changHouseValues(ctx, houseID, price, location, type, owner) {
+
+        const houseAsBytes = await ctx.stub.getState(houseID);
+        if (!houseAsBytes || houseAsBytes.length === 0) {
+            throw new Error(`${houseID} does not exist`);
+        }
+        const house = JSON.parse(houseAsBytes.toString());
+        house.price = (price !== '') ? price : house.price;
+        house.location = (location !== '') ? location : house.location;
+        house.type = (type !== '') ? type : house.type;
+        house.owner = (owner !== '') ? owner : house.owner;
+
+        await ctx.stub.putState(houseID, Buffer.from(JSON.stringify(house)));
+    }
+
+}
+
+module.exports = Application;
